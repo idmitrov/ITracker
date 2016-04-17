31ebe2626a2ce7357524bd64a02164ce55b8f024
idmitrov <mailto.dimitrov@gmail.com>
2016-04-17 14:42:52 +0300

    implement requestingService

---
+++ app/js/services/requestingService.js
@@ -0,0 +1,22 @@
+(function() {
+	function requestingrService($q, $http) {
+		return function(method, url, headers, data) {
+			var deferred = $q.defer();
+
+			$http({
+				method: method,
+				url: url,
+				headers: headers,
+				data: data
+			})
+				.success(deferred.resolve)
+				.error(deferred.reject);
+			
+			return deferred.promise;
+		}
+	}
+
+	angular
+		.module('ITracker')
+		.factory('requestingrService', ['$q', '$http', requestingrService]);
+} ());
